a6814c7029a29e8760c703586953584185ba0934
x256 <153709773+xTwo56@users.noreply.github.com>
2024-04-11 00:05:56 +0530

    feat: created function to set details and view assets

--- contracts/net_worth_mate.sol
+++ contracts/net_worth_mate.sol
@@ -1,3 +1,4 @@
+
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
 
@@ -14,7 +15,28 @@ contract net_worth_mate
     }
 
     mapping (address => AssetDetails) public assetMapping;
-    function setDetails() public {
+    
+    modifier isPositive5(uint num1, uint num2, uint num3, uint num4, uint num5){
+        require(num1>0 && num2>0 && num3>0 && num4>0 && num5>0, "Invalid Input");
+        _;
+    }
+
+    function setDetails(
+        uint houses,
+        uint jewellery,
+        uint cars,
+        uint land,
+        uint miscellaneous) external isPositive5(houses, jewellery, cars, land, miscellaneous) {
+     
+        AssetDetails memory assets = AssetDetails(houses, jewellery, cars, land, miscellaneous);
+        assetMapping[msg.sender] = assets;    
 
     }
+
+    function viewAssets()public view returns(uint[5] memory){
+        AssetDetails memory assets = assetMapping[msg.sender];
+        uint[5] memory assetArray = [assets.houses, assets.jewellery, assets.cars, assets.land,  assets.miscellaneous];
+        return assetArray;
+    }
 }
+
