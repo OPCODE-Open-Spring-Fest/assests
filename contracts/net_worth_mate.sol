9d91c5ca197b20b143e913c964436e4737e05d7d
x256 <153709773+xTwo56@users.noreply.github.com>
2024-04-11 00:57:05 +0530

    feat: created modifier which checks the existence of the user

--- contracts/net_worth_mate.sol
+++ contracts/net_worth_mate.sol
@@ -1,4 +1,5 @@
 
+
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
 
@@ -12,12 +13,18 @@ contract net_worth_mate
         uint cars;
         uint land;
         uint miscellaneous;
+        bool exist;
     }
 
     mapping (address => AssetDetails) public assetMapping;
     
     modifier isPositive5(uint num1, uint num2, uint num3, uint num4, uint num5){
         require(num1>0 && num2>0 && num3>0 && num4>0 && num5>0, "Invalid Input");
+        _;
+    }
+    
+    modifier userPresent(){
+        require(assetMapping[msg.sender].exist, "User not found");
         _;
     }
 
@@ -28,21 +35,22 @@ contract net_worth_mate
         uint land,
         uint miscellaneous) external isPositive5(houses, jewellery, cars, land, miscellaneous) {
      
-        AssetDetails memory assets = AssetDetails(houses, jewellery, cars, land, miscellaneous);
+        AssetDetails memory assets = AssetDetails(houses, jewellery, cars, land, miscellaneous, true);
         assetMapping[msg.sender] = assets;    
 
     }
 
-    function viewAssets()public view returns(uint[5] memory){
+    function viewAssets()public view userPresent returns(uint[5] memory){
         AssetDetails memory assets = assetMapping[msg.sender];
         uint[5] memory assetArray = [assets.houses, assets.jewellery, assets.cars, assets.land,  assets.miscellaneous];
         return assetArray;
     }
 
-    function checkNetWorth() external view returns(uint){
+    function checkNetWorth() external view userPresent returns(uint){
         uint[5] memory assetArray = viewAssets();
         uint netWorth = assetArray[0] + assetArray[1] + assetArray[2] + assetArray[3] + assetArray[4];
         return netWorth;
     }
 }
+
 
